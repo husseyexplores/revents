4330f0ed455cdd744b460c34335bd9819318e4e3
husseyexplores <husseyexplores@gmail.com>
2019-03-04 00:40:41 +0500

    Redux setup and integration

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,34 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
+import rootReducer from '../reducers'
+
+const configureStore = preloadedState => {
+  const middlewares = [thunk]
+  const middlewareEnahcer = applyMiddleware(...middlewares)
+
+  const reduxDevToolsInitializer =
+    typeof window === 'object' &&
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+
+  const storeEnhancers = [middlewareEnahcer, reduxDevToolsInitializer]
+
+  const composedEnhancer = compose(...storeEnhancers)
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancer)
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+      module.hot.accept('../reducers', () => {
+        const newRootReducer = require('../reducers/').default
+        store.replaceReducer(newRootReducer)
+      })
+    }
+  }
+
+  return store
+}
+
+export default configureStore
--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import 'semantic-ui-css/semantic.min.css'
+
+import { configureStore } from './app/store'
 import './index.css'
 import App from './app/layout/App'
+
 import * as serviceWorker from './serviceWorker'
+
+const store = configureStore()
 
 const rootEl = document.getElementById('root')
 
 function render() {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   )
 }
-/*
+
 if (module.hot) {
   module.hot.accept('./app/layout/App', () => {
     setTimeout(render)
   })
 }
-*/
 
 render()
 
