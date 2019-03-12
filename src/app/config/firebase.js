59f46ddc85886b2b560f12878c3f712f90d48b0f
husseyexplores <husseyexplores@gmail.com>
2019-03-12 17:56:08 +0500

    Initialize firebase storage

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,11 +1,13 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 import { firebaseConfig } from '../../secret'
 
 firebase.initializeApp(firebaseConfig)
 firebase.firestore()
 firebase.auth()
+firebase.storage()
 
 export default firebase
