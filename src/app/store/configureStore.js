4debc0933eb7517a2886fbd0eb9aea6b8762a729
husseyexplores <husseyexplores@gmail.com>
2019-03-15 11:17:19 +0500

    change formMiddleware file location

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import formMiddleware from '../common/util/formMiddleware'
+import formMiddleware from './formMiddleware'
 
 import firebase from '../config/firebase'
 
@@ -13,11 +13,13 @@ const configureStore = preloadedState => {
 
   // not working with redux compose and with reactReduxFirebase `attachAuthIsReady`
   // Need to debug.
+  /*
   const reduxDevToolsInitializer =
     typeof window === 'object' &&
     typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : f => f
+  */
 
   // const storeEnhancers = [middlewareEnahcer, reduxDevToolsInitializer]
   const storeEnhancers = [middlewareEnahcer]
