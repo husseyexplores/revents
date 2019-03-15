5d1572a389207cfac96468ff56510ac7dad4ae90
husseyexplores <husseyexplores@gmail.com>
2019-03-15 15:11:00 +0500

    Convert to functional component

--- src/features/event/EventDetailed/EventDetailedInfo.js
+++ src/features/event/EventDetailed/EventDetailedInfo.js
@@ -1,74 +1,67 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Segment, Grid, Icon, Button } from 'semantic-ui-react'
 
 import EventDetailedMap from './EventDetailedMap'
 
-class EventDetailedInfo extends Component {
-  state = {
-    showMap: false,
+function EventDetailedInfo({ description, date, venue, lat, lng }) {
+  const [showMap, setShowMap] = useState(false)
+
+  function toggleShowMap() {
+    setShowMap(!showMap)
   }
 
-  toggleShowMap = () => {
-    this.setState(state => ({ showMap: !state.showMap }))
-  }
-
-  render() {
-    const { description, date, venue, lat, lng } = this.props
-    const { showMap } = this.state
-
-    return (
-      <Segment.Group>
-        <Segment attached="top">
-          <Grid>
-            <Grid.Column width={1}>
-              <Icon size="large" color="teal" name="info" />
-            </Grid.Column>
-            <Grid.Column width={15}>
-              <p>{description}</p>
-            </Grid.Column>
-          </Grid>
-        </Segment>
-        <Segment attached>
-          <Grid verticalAlign="middle">
-            <Grid.Column width={1}>
-              <Icon name="calendar alternate" size="large" color="teal" />
-            </Grid.Column>
-            <Grid.Column width={15}>
-              <span>{date}</span>
-            </Grid.Column>
-          </Grid>
-        </Segment>
-        <Segment attached>
-          <Grid verticalAlign="middle">
-            <Grid.Column width={1}>
-              <Icon name="map marker alternate" size="large" color="teal" />
-            </Grid.Column>
-            <Grid.Column width={11}>
-              <span>{venue}</span>
-            </Grid.Column>
-            <Grid.Column width={4}>
-              <Button
-                color="teal"
-                size="tiny"
-                content={`${showMap ? 'Hide' : 'Show'} Map`}
-                onClick={this.toggleShowMap}
-              />
-            </Grid.Column>
-          </Grid>
-        </Segment>
-        {showMap && <EventDetailedMap lat={lat} lng={lng} />}
-      </Segment.Group>
-    )
-  }
+  return (
+    <Segment.Group>
+      <Segment attached="top">
+        <Grid>
+          <Grid.Column width={1}>
+            <Icon size="large" color="teal" name="info" />
+          </Grid.Column>
+          <Grid.Column width={15}>
+            <p>{description}</p>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+      <Segment attached>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={1}>
+            <Icon name="calendar alternate" size="large" color="teal" />
+          </Grid.Column>
+          <Grid.Column width={15}>
+            <span>{date}</span>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+      <Segment attached>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={1}>
+            <Icon name="map marker alternate" size="large" color="teal" />
+          </Grid.Column>
+          <Grid.Column width={11}>
+            <span>{venue}</span>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <Button
+              color="teal"
+              size="tiny"
+              content={`${showMap ? 'Hide' : 'Show'} Map`}
+              onClick={toggleShowMap}
+            />
+          </Grid.Column>
+        </Grid>
+      </Segment>
+      {showMap && <EventDetailedMap lat={lat} lng={lng} />}
+    </Segment.Group>
+  )
 }
 
 EventDetailedInfo.propTypes = {
   description: PropTypes.string.isRequired,
   date: PropTypes.any.isRequired,
   venue: PropTypes.string.isRequired,
+  lat: PropTypes.number.isRequired,
+  lng: PropTypes.number.isRequired,
 }
-
-EventDetailedInfo.defaultProps = {}
 
 export default EventDetailedInfo
